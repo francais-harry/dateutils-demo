842b61d9518943be56a7599d87c8243236478e95
Hisato Shoji <francais.harry@gmail.com>
2016-06-12 15:09:13 +0900

    Appied data binding framework

--- app/src/main/java/jp/f/dev/android/sample/dateutils/MainActivity.java
+++ app/src/main/java/jp/f/dev/android/sample/dateutils/MainActivity.java
@@ -1,13 +1,18 @@
 package jp.f.dev.android.sample.dateutils;
 
-import android.support.v7.app.AppCompatActivity;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import jp.f.dev.android.sample.dateutils.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = DataBindingUtil
+                .setContentView(this, R.layout.activity_main);
+
     }
 }
